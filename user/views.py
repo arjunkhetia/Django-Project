c479033a7be0205b3d296e49dcd4ab517d7f0d51
Arjun Khetia <arjun@perceptioncare.com>
2025-02-10 19:13:25 +0530

    404 Error

--- user/views.py
+++ user/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def user(request):
     return HttpResponse("User works.")
 
 def userid_int(request, id):
-    return HttpResponse(f'User by id : {id} works. ID is Integer')
+    if id == 404:
+        return HttpResponseNotFound('404 ID is not allowed')
+    else :
+        return HttpResponse(f'User by id : {id} works. ID is Integer')
 
 def userid_str(request, id):
     return HttpResponse(f'User by id : {id} works. ID is String')
